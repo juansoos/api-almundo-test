a9b32b9bf7d8511c01689861f92988b107e8cac7
juansoos <juliansotelo0825@gmail.com>
2018-02-19 08:15:29 -0500

    Se añade la clase de base de datos y las rutas de hoteles

--- config/index.js
+++ config/index.js
@@ -1,2 +1,9 @@
 export const port = process.env.PORT || 3000;
 export const secret = process.env.SECRET || 'eyeJ0eXAiOi';
+export const db = {
+  user: 'admin',
+  pwd: 'Od5Z524CmNoq',
+  host: 'ds243285.mlab.com',
+  port: '43285',
+  db: 'almundo',
+};
--- routes/hotel.js
+++ routes/hotel.js
@@ -1,23 +1,103 @@
 import express from 'express';
 import Debug from 'debug';
-import jwt from 'express-jwt';
+import Db from '../database';
+import { db } from '../config';
+import { handleError } from '../utils';
 
 const app = express.Router();
+const database = new Db(db);
 const debug = new Debug('almundo-api:hotel');
 
 // Ruta para obtener todos los hoteles
-app.get('/hotels');
+app.get('/hotels', async (req, res) => {
+  debug('Get hotels');
+  try {
+    await database.connect();
+    const hotels = await database.getHotels();
+    await database.disconnect();
+    res.status(200).json(hotels);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
 // Ruta para buscar hoteles por nombre
-app.get('/hotels/name/:name');
+app.get('/hotels/name/:name', async (req, res) => {
+  debug(`Get hotels by name ${req.params.name}`);
+  const { name } = req.params;
+  try {
+    await database.connect();
+    const hotels = await database.getHotelsByName(name);
+    await database.disconnect();
+    res.status(200).json(hotels);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
 // Ruta para buscar hoteles por estrellas
-app.get('/hotels/stars/:stars');
+app.get('/hotels/stars/:stars', async (req, res) => {
+  debug(`Get hotel by stars ${req.params.stars}`);
+  const { stars } = req.params;
+  try {
+    await database.connect();
+    const hotels = await database.getHotelsByStars(parseInt(stars, 10));
+    await database.disconnect();
+    res.status(200).json(hotels);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
 // Ruta para obtener un hotel
-app.get('/hotel/:id');
+app.get('/hotel/:id', async (req, res) => {
+  debug(`Get hotel by id ${req.params.id}`);
+  const { id } = req.params;
+  try {
+    await database.connect();
+    const hotels = await database.getHotels(id);
+    await database.disconnect();
+    res.status(200).json(hotels);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
 // Ruta para crear un hotel
-app.post('/hotel');
+app.post('/hotel', async (req, res) => {
+  debug('Creating new hotel');
+  const { hotel } = req.body;
+  try {
+    await database.connect();
+    const hotelCreated = await database.createHotel(hotel);
+    await database.disconnect();
+    res.status(201).json(hotelCreated);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
 // Ruta para modificar un hotel
-app.put('/hotel/:id');
+app.put('/hotel/:id', async (req, res) => {
+  debug(`Updating hotel by id ${req.params.id}`);
+  const { id } = req.params;
+  const { hotel } = req.body;
+  try {
+    await database.connect();
+    const hotelUpdated = await database.updateHotel(id, hotel);
+    await database.disconnect();
+    res.status(201).json(hotelUpdated);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
 // Ruta para eliminar un hotel
-app.delete('/hotel/:id');
+app.delete('/hotel/:id', async (req, res) => {
+  debug(`Deleting hotel by id ${req.params.id}`);
+  const { id } = req.params;
+  try {
+    await database.connect();
+    const hotelDeleted = await database.deleteHotel(id);
+    await database.disconnect();
+    res.status(201).json(hotelDeleted);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
 
 export default app;
